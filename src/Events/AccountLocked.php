1560d0fb2b71ff56cbb236bd0aa0c3a9f3fe53ab
jery123 <jerykhenson@gmail.com>
2026-09-23 00:17:45 +0530

    fix: pass unlocksAt parameter to AccountLocked event

--- src/Events/AccountLocked.php
+++ src/Events/AccountLocked.php
@@ -4,15 +4,18 @@ namespace LaraCare\AccountLockout\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use DateTimeInterface;
 
 class AccountLocked
 {
     use Dispatchable, SerializesModels;
 
     public mixed $user;
+    public ?DateTimeInterface $unlocksAt;
 
-    public function __construct(mixed $user)
+    public function __construct(mixed $user, ?DateTimeInterface $unlocksAt = null)
     {
         $this->user = $user;
+        $this->unlocksAt = $unlocksAt;
     }
 }
